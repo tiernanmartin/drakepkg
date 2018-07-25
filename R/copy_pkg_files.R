5054d2acf342e704721920dd86b7452545a9b4b3
Tiernan Martin <tiernan.martin@gmail.com>
2018-07-25 13:45:47 -0700

    create a function for copying the project files into the working directory   * or some other specified folder

---
+++ R/copy_pkg_files.R
@@ -0,0 +1,28 @@
+#' @title Save the Source Code Files of a Packaged Drake Workflow.
+#' @description Copy the files of a packaged \code{drake} workflow into a
+#'   supplied directory.
+#' @param to Character scalar, file path, where
+#'   to write the folders containing the workflow's files.
+#' @param overwrite Logical, whether to overwrite any existing folders
+#'   with the same name as the workflow folders.
+#' @return `NULL`
+#' @importFrom purrr map
+#' @importFrom purrr walk
+#' @export
+copy_pkg_files <- function(to = getwd(), overwrite = FALSE){
+
+  dir_list <- map(
+    list("extdata", "doc"),
+    system.file,
+    package = "drakepkg",
+    mustWork = TRUE)
+
+  walk(dir_list,
+       file.copy,
+       to = to,
+       overwrite = overwrite,
+       recursive = TRUE)
+
+  invisible()
+
+}
