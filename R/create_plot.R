47d5a59c5e8db1577278eb5d40e58cee96fa41ac
Tiernan Martin <tiernan.martin@gmail.com>
2018-07-25 13:43:35 -0700

    add imported ggplot2 functions

--- R/create_plot.R
+++ R/create_plot.R
@@ -2,13 +2,16 @@
 #' @description Create a specific histogram for the \code{iris} data
 #' @param data the \code{iris} dataset
 #' @name create_plot
-#' @import ggplot2
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 geom_histogram
+#' @importFrom ggplot2 theme_gray
 #' @return a \link{ggplot2} plot object
 create_plot <- function(data) {
 
-  hist_plot <- ggplot2::ggplot(data, aes(x = Petal.Width, fill = Species)) +
-    ggplot2::geom_histogram(binwidth = 0.25) +
-    ggplot2::theme_gray(20)
+  hist_plot <- ggplot(data, aes(x = Petal.Width, fill = Species)) +
+    geom_histogram(binwidth = 0.25) +
+    theme_gray(20)
 
   return(hist_plot)
 }
