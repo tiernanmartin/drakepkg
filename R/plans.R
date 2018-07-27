7b2220d6f6e8de567a57f2d58c4e3b7793306fb0
Tiernan Martin <tiernan.martin@gmail.com>
2018-07-27 09:53:03 -0700

    improve documentation for plan_example

--- R/plans.R
+++ R/plans.R
@@ -1,22 +1,33 @@
 #' @title An Example of a Drake Plan
-#' @description An example of a drake plan that contains several targets
+#' @description An example of a \code{\link[drake]{drake_plan}} that contains several targets
 #'   and commands, including processing input and output files.
-#' @format A \code{drake::\link[drake]{drake_plan}} object (\code{class=data.frame}) with the following targets:
+#' @format A \code{\link[drake]{drake_plan}} object of class \code{tbl_df} with the following \code{\link[drake]{target}}s:
 #'   \describe{
 #'
 #'   \item{`raw_data`:}{`iris` data from an .xlsx file}
-#'   \item{`ready_data`:}{ a prepared version of the `raw_data`}
-#'   \item{`hist`:}{ a histogram plot created with `ggplot2`}
-#'   \item{`fit`:}{ a linear model of the data (`Sepal.Width` as a function of `Petal.Width` and `Species`)}
-#'   \item{`report`}{a html report document}
+#'   \item{`ready_data`:}{a prepared version of the `raw_data`}
+#'   \item{`hist`:}{a histogram plot created with `ggplot2`}
+#'   \item{`fit`:}{a linear model of the data (`Sepal.Width` as a function of `Petal.Width` and `Species`)}
+#'   \item{`report`:}{a html report document}
 #'
-#'   These targets can be accessed using \code{drake::\link[drake]{loadd}}
-#'     (e.g., `drake::loadd(fit)`).
 #'   }
 #'
 #' @keywords drake, plan
 #' @examples
-#'   \dontrun{
-#'     drake::make(plan_example)
-#'    }
+#'
+#' # Print the plan
+#'
+#' print(plan_example)
+#'
+#'
+#' # Make the plan, load a target, print the target
+#'
+#' \dontrun{
+#'
+#' make(plan_example)
+#'
+#' loadd(fit)
+#'
+#' print(fit)
+#' }
 "plan_example"
