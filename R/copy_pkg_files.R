9e03368811a25384acc2170168c6c7c584e905bf
Tiernan Martin <tiernan.martin@gmail.com>
2018-07-29 13:50:31 -0700

    improve documentation for copy_pkg_files()

--- R/copy_pkg_files.R
+++ R/copy_pkg_files.R
@@ -1,10 +1,19 @@
-#' @title Save the Source Code Files of a Packaged Drake Workflow.
+#' @title Save the Source Code Files of a Packaged Drake Workflow
 #' @description Copy the files of a packaged \code{drake} workflow into a
 #'   supplied directory.
 #' @param to Character scalar, file path, where
 #'   to write the folders containing the workflow's files.
 #' @param overwrite Logical, whether to overwrite any existing folders
 #'   with the same name as the workflow folders.
+#' @note This function will copy the following directories (and their subdirectories and files) into
+#'   the user's working directory (or the directory supplied to the \code{to} parameter): \cr
+#'   `
+#'    . \cr
+#'    ├── doc/ \cr
+#'    ├── extdata/ \cr
+#'    └── plans/ \cr
+#'   `
+#'
 #' @return `NULL`
 #' @importFrom purrr map
 #' @importFrom purrr walk
@@ -12,7 +21,7 @@
 copy_pkg_files <- function(to = getwd(), overwrite = FALSE){
 
   dir_list <- map(
-    list("extdata", "doc", "plans"),
+    list("doc", "extdata", "plans"),
     system.file,
     package = "drakepkg",
     mustWork = TRUE)
