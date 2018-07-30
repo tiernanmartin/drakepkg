0daf7ebe5f86ff5ff68459fc7e11c01eeff2a98b
Tiernan Martin <tiernan.martin@gmail.com>
2018-07-29 17:14:15 -0700

    make a minor edit to external_data_list documentation

--- R/data.R
+++ R/data.R
@@ -14,3 +14,28 @@
 #'
 #' @source \url{https://osf.io/u4kb7/}
 "otheriris"
+
+#' @title External Data List
+#' @description This is a list of datasets that are included in the package workflow but
+#'   are not stored in the \code{data/} directory.
+#'
+#'   The primary reason that a dataset is included in this list is that it exceeds Github's
+#'   50 MB file size limit (see \href{https://help.github.com/articles/conditions-for-large-files/}{here} for details).
+#'
+#' @docType data
+#' @format The data itself is simply a \code{NULL} value.
+#'
+#'  Its purpose is to provide some documentation of the datasets that are not directly
+#'  included in the package (use \code{help(external_data_list)} to access it).
+#'
+#'  The following external datasets are accessed by workflows in this package:
+#'
+#' \describe{
+#'
+#'   \item{`• example_one`:}{Brief description (\href{https://osf.io/u4kb7/}{link})}
+#'   \item{`• example_two`:}{Brief description (\href{https://osf.io/u4kb7/}{link})}
+#'   \item{`• example_three`:}{Brief description (\href{https://osf.io/u4kb7/}{link})}
+#'
+#'   }
+#'
+"external_data_list"
