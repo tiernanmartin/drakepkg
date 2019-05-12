98d055dd8bd311b0efe5cdc962fc85beb046e095
Tiernan Martin <tiernan.martin@gmail.com>
2019-05-12 16:52:08 -0700

    add the second vignette (how to make a package w/ a drake workflow)

---
+++ inst/doc/drakepkg-02.R
@@ -0,0 +1,9 @@
+## ----setup, include = FALSE----------------------------------------------
+  knitr::opts_chunk$set(
+  collapse = TRUE,
+  fig.path = "man/figures/vignette-",
+  comment = "#>"
+  )
+  options(drake_make_menu = FALSE,
+  drake_clean_menu = FALSE)
+
